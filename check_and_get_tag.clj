b2ff635dad4a17dedb7b720a1f4784438d331913
mntcloud <vadikvoskr@gmail.com>
2024-02-07 19:38:20 +0200

    fix: comparision of qemu's website and github tag

--- check_and_get_tag.clj
+++ check_and_get_tag.clj
@@ -6,13 +6,15 @@
     (last (last (re-seq #"qemu-([\w\.\-]+)\.tar\.xz(?=<)" (:body resp))))
     nil))
 
+(defn extract-github-tag [resp] 
+  (second (first (first (json/parse-string (:body resp))))))
+
 (if-let [resp (http/get 
                "https://api.github.com/repos/ktzh/qemu-binaries/tags?per_page=1" 
                {:as :text})]  
   (if-let [qemu-version (get-latest-qemu-version)]
-    ; from the right side of =, we're getting the name key
-    (if (= qemu-version (second (first (first (json/parse-string (:body @resp)))))) 
+    (if (= (str "v" qemu-version) (extract-github-tag @resp)) 
       (System/exit 1) ;; if it matches stop the job, because we already have the latest release 
-      (println qemu-version)) ;; if it doesn't (maybe we don't have any tags, for example) continue the job
+      (print qemu-version)) ;; if it doesn't (maybe we don't have any tags, for example) continue the job
     (System/exit 1)) ;; in all other cases just exit with code 1 
   (System/exit 1))
