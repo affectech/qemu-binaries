41922c5e7ce70f0329e0b2a48f24b60916afb30a
mntcloud <vadikvoskr@gmail.com>
2024-02-07 20:03:39 +0200

    fix: handling of empty tag version

--- check_and_get_tag.clj
+++ check_and_get_tag.clj
@@ -15,6 +15,6 @@
   (if-let [qemu-version (get-latest-qemu-version)]
     (if (= (str "v" qemu-version) (extract-github-tag @resp)) 
       (System/exit 1) ;; if it matches stop the job, because we already have the latest release 
-      (print qemu-version)) ;; if it doesn't (maybe we don't have any tags, for example) continue the job
+      (print (str "v" qemu-version))) ;; if it doesn't (maybe we don't have any tags, for example) continue the job
     (System/exit 1)) ;; in all other cases just exit with code 1 
   (System/exit 1))
